beedb8f758e7bed43705e38523dcd6d9c8240a74
hasmeen <112644288+hasmeen@users.noreply.github.com>
2023-01-05 11:28:09 -0500

    commit

--- list_slicing.py
+++ list_slicing.py
@@ -4,57 +4,51 @@ def main():
     names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen”",
     "Jackie", "Kurt", "Linda"]
 
-    # Example: print the first three names from the list
-    # Answer print(names[0:3])
+   # The list below will be used for all of the exercises below:
 
-    # 1. Print ['Doug', 'Emma']
+# Example: print the first three names from the list
+# Answer print(names[0:3])
 
+# 1. Print ['Doug', 'Emma']
+print(names[3:5])
 
+# 2. Print [‘Brenda’, ‘Chad’, ‘Doug’, ‘Emma’, ‘Francis’]
+print(names[1:6])
 
-    # 2. Print [‘Brenda’, ‘Chad’, ‘Doug’, ‘Emma’, ‘Francis’]
+# 3. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using two numbers in the slice)
+print(names[5:12])
 
+# 4. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using one number in the slice)
+print(names[5:])
 
+# 5. [‘Linda’] (using a positive number)
+print(names[11])
 
-    # 3. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using two numbers in the slice)
+# 6. [‘Linda’] (using a negative number)
+print(names[-1])
 
+# 7. [‘Brenda’, ‘Doug’, ‘Francis’, ‘Harold’]
+print(names[1:8:2])
 
-    # 4. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using one number in the slice)
-
-
-
-    # 5. [‘Linda’] (using a positive number)
-
-
-    # 6. [‘Linda’] (using a negative number)
-
-
-
-    # 7. [‘Brenda’, ‘Doug’, ‘Francis’, ‘Harold’]
-
-
-
-    # 8. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’]
+# 8. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’]
+print(names[0:11:2])
 
 
+# 9. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’] (using a different way than above.
+# HINT: If you leave a slice number blank, the default is either the beginning or the end of the list,
+# depending on which side of the colon is blank.
+print(names[:11:2])
 
-    # 9. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’] (using a different way than above.
-    # HINT: If you leave a slice number blank, the default is either the beginning or the end of the list,
-    # depending on which side of the colon is blank.
-
-
-    # 10. [‘Linda’, ‘Kurt’, ‘Jackie’]
-
-
-    # 11. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
+# 10. [‘Linda’, ‘Kurt’, ‘Jackie’]
+print(names[11:8:-1])
 
 
+# 11. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
+print(names[-1::-1])
 
-    # 12. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
-    # (in a different way than 11. See hint for 9 for help.)
-
-
-
-
+# 12. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
+# (in a different way than 11. See hint for 9 for help.)
+print(names[11::-1])
 
 if __name__ == '__main__':
     main()
